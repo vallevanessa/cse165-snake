91dff7ade769efbf1b599d835bc43f9377e82cde
Pow <eddiecampuzano@gmail.com>
2024-04-25 23:40:50 -0700

    Snake now properly collides with itself and gameover occurs

--- BasicOpenGLProject/main.cpp
+++ BasicOpenGLProject/main.cpp
@@ -1,6 +1,4 @@
 #include <GL/freeglut.h>
-//#include <external/GL/freeglut.h>
-//#include <../external/freeglut/include/GL/freeglut.h>
 #include <iostream>
 #include <vector>
 #include <cstdlib>
@@ -15,9 +13,6 @@
 // http://freeglut.sourceforge.net/docs/api.php#WindowCallback
 //-----------------------------------------------------------------------------
 
-
-
-#include <cstdlib> // for exit()
 
 void gameOver() {
 
@@ -38,34 +33,59 @@ class Snake {
 private:
 	std::vector<SnakeSegment> segment;
 	char direction;
+	static const int segmentSize = 30;
+	float r, g, b;
 public:
-	Snake(int startX, int startY) {
+	Snake(int startX, int startY) : r(1.0f), g(1.0f), b(1.0f) {
 		segment.push_back({ startX, startY });
 		direction = 'd';
 	}
+
+	~Snake() {}
+
 	void move() {
 		int newX = segment.front().x;
 		int newY = segment.front().y;
 
 		switch (direction) {
 		case 'w':
-			newY++;
+			newY += segmentSize;
 			break;
 
 		case 'a':
-			newX--;
+			newX -= segmentSize;
 			break;
 		case 's':
-			newY--;
+			newY -= segmentSize;
 			break;
 
 		case 'd':
-			newX++;
+			newX += segmentSize;
 			break;
 		}
 
-		segment.pop_back();
 		segment.insert(segment.begin(), { newX, newY });
+		segment.pop_back();
+	}
+
+	void grow() {
+		int tailX = segment.back().x;
+		int tailY = segment.back().y;
+
+		switch (direction) {
+		case 'w':
+			segment.push_back({ tailX, tailY + segmentSize });
+			break;
+		case 'a':
+			segment.push_back({ tailX + segmentSize, tailY });
+			break;
+		case 's':
+			segment.push_back({ tailX, tailY - segmentSize });
+			break;
+		case 'd':
+			segment.push_back({ tailX - segmentSize, tailY });
+			break;
+		}
 	}
 
 	void setDirection(char newDirection) {
@@ -75,6 +95,17 @@ public:
 		}
 	}
 
+	void changeColorToRandom() {
+		r = static_cast<float>(rand()) / static_cast<float>(RAND_MAX);
+		g = static_cast<float>(rand()) / static_cast<float>(RAND_MAX);
+		b = static_cast<float>(rand()) / static_cast<float>(RAND_MAX);
+
+	}
+
+	float getRed() const { return r; }
+	float getGreen() const { return g; }
+	float getBlue() const { return b; }
+
 	std::vector<SnakeSegment> getSegments() const {
 		return segment;
 	}
@@ -83,36 +114,43 @@ public:
 
 
 
-Snake snake(400, 300);
+Snake snake(405, 315);
 
-const int snakeSpeed = 15;
+int snakeSpeed = 95;
 
 class GameObject {
 public:
 	virtual void draw() const = 0;
-	virtual void playSound() const = 0;
+	virtual void foodEffect() const = 0;
+	virtual ~GameObject() {}
 };
 
 
 class Food : public GameObject {
 protected:
 	int x, y;
-public: 
+	static const int segmentSize = 30;
+public:
 	Food() : x(0), y(0) {}
+	~Food() {}
 	virtual void placeRandom(int maxX, int maxY) {
-		x = rand() % maxX;
-		y = rand() % maxY;
+		int cellX = rand() % (maxX / segmentSize);
+		int cellY = rand() % (maxY / segmentSize);
+
+		// Set food position to the center of the chosen cell
+		x = cellX * segmentSize + segmentSize / 2;
+		y = cellY * segmentSize + segmentSize / 2;
 	}
 
 	virtual void draw() const = 0;
-	virtual void playSound() const = 0;
+	virtual void foodEffect() const = 0;
 	virtual int getX() const { return x; }
 	virtual int getY() const { return y; }
 };
 
 
 class Apple : public Food {
-public: 
+public:
 	void draw() const override {
 		const float size = 30.0f;
 		glColor3f(1.0f, 0.0f, 0.0f);
@@ -124,9 +162,11 @@ public:
 		glEnd();
 	}
 
-	void playSound() const override {
-		std::cout << "Eating apple sound \n";  // to be replaced!!
+	void foodEffect() const override {
+		snakeSpeed -= 5;
+		snake.grow();
 	}
+	~Apple() {}
 };
 
 class Orange : public Food {
@@ -142,9 +182,11 @@ public:
 		glEnd();
 	}
 
-	void playSound() const override {
-		std::cout << "Eating orange sound \n";  // to be replaced!!
+	void foodEffect() const override {
+		snakeSpeed += 5;
+		snake.grow();
 	}
+	~Orange() {}
 };
 
 class Grape : public Food {
@@ -160,9 +202,12 @@ public:
 		glEnd();
 	}
 
-	void playSound() const override {
-		std::cout << "Eating grape sound \n";  // to be replaced!!
+	void foodEffect() const override {
+		snake.grow();
+		snake.grow();
+		snake.grow();
 	}
+	Grape() {}
 };
 
 
@@ -179,50 +224,21 @@ public:
 		glEnd();
 	}
 
-	void playSound() const override {
-		std::cout << "Eating banana sound \n";  // to be replaced!!
+	void foodEffect() const override {
+		snake.changeColorToRandom();
+		snake.grow();
 	}
+	~Banana() {}
 };
 
-Food* food;
+Food* food = nullptr;
 
-void checkWallCollision();
+const int segmentSize = 30;
 
-
-void update(int value) {
-	snake.move();
-	checkWallCollision(); // added this line to check if the snake hit the wall.
-
-	if (snake.getSegments().front().x == food->getX() && snake.getSegments().front().y == food->getY()) {
-		food->playSound();
-
-		snake.getSegments().push_back(snake.getSegments().back());
-
-		delete food;
-
-		switch (rand() % 4) {
-		case 0:
-			food = new Apple();
-			break;
-
-		case 1:
-			food = new Orange();
-			break;
-		case 2: 
-			food = new Grape();
-			break;
-		case 3: 
-			food = new Banana();
-			break;
-		}
-		food->placeRandom(800, 600);
-	}
-
-	glutPostRedisplay();
-	glutTimerFunc(snakeSpeed, update, 0);
+bool isCollision(int x1, int y1, int x2, int y2) {
+	return x1 == x2 && y1 == y2;
 }
 
-//the game doesnt end when the tip of the snake hits the wall currently, it ends when the middle of the snake hits. at least that what its looks like to me. I will think about a solution to that for now but i think it looks good anyways.
 void checkWallCollision() {
 	//get position of the snakes head.
 	int headX = snake.getSegments().front().x;
@@ -240,16 +256,71 @@ void checkWallCollision() {
 	}
 }
 
+void checkSelfCollision() {
+	// Get the position of the snake's head
+	int headX = snake.getSegments().front().x;
+	int headY = snake.getSegments().front().y;
+
+	// Store the segments of the snake's body in a local variable
+	const auto& segments = snake.getSegments();
+
+	// Iterate through all segments of the snake's body except for the head
+	for (auto it = std::next(segments.begin()); it != segments.end(); ++it) {
+		// Check if the head collides with any other segment of the body
+		if (headX == it->x && headY == it->y) {
+			gameOver(); // Trigger game over if self-collision is detected
+			return;
+		}
+	}
+}
+
+void update(int value) {
+	snake.move();
+	checkWallCollision(); // added this line to check if the snake hit the wall.
+	checkSelfCollision();
+
+	if (isCollision(snake.getSegments().front().x, snake.getSegments().front().y,
+		food->getX(), food->getY())) {
+		food->foodEffect();
+
+		//	snake.grow();
+
+		delete food;
+
+		switch (rand() % 4) {
+		case 0:
+			food = new Apple();
+			break;
+
+		case 1:
+			food = new Orange();
+			break;
+		case 2:
+			food = new Grape();
+			break;
+		case 3:
+			food = new Banana();
+			break;
+		}
+		food->placeRandom(800, 600);
+	}
+
+	glutPostRedisplay();
+	glutTimerFunc(snakeSpeed, update, 0);
+}
+
+
+
 void idle_func()
 {
 	//snake.move();
 	//uncomment below to repeatedly draw new frames
 	//glutPostRedisplay();
 }
- 
-void reshape_func( int width, int height )
+
+void reshape_func(int width, int height)
 {
-	glViewport( 0, 0, width, height );
+	glViewport(0, 0, width, height);
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
 	gluOrtho2D(0, width, 0, height);
@@ -258,9 +329,9 @@ void reshape_func( int width, int height )
 	glutPostRedisplay();
 }
 
-void keyboard_func( unsigned char key, int x, int y )
+void keyboard_func(unsigned char key, int x, int y)
 {
-	switch( key )
+	switch (key)
 	{
 	case 'w':
 	{
@@ -294,7 +365,7 @@ void keyboard_func( unsigned char key, int x, int y )
 	}
 	}
 
-	snake.move();
+	//snake.move();
 
 	glutPostRedisplay();
 }
@@ -332,11 +403,36 @@ void display_func(void)
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 	glLoadIdentity();
 
-	food->draw();
+	//grid
+	glColor3f(0.0f, 0.0f, 0.0f);
+
+	const float lineWidth = 2.0f;
+
+	for (int x = 0; x <= 800; x += segmentSize) {
+		glLineWidth(lineWidth);
+		glBegin(GL_LINES);
+		glVertex2f(x, 0);
+		glVertex2f(x, 600);
+		glEnd();
+
+	}
+	for (int y = 0; y <= 600; y += segmentSize) {
+		glLineWidth(lineWidth);
+		glBegin(GL_LINES);
+		glVertex2f(0, y);
+		glVertex2f(800, y);
+		glEnd();
+
+	}
+
+
+	if (food) {
+		food->draw();
+	}
 
 	//draw snake
 	const float size = 30.0f;
-	glColor3f(1.0f, 1.0f, 1.0f); //snake color (change)
+	glColor3f(snake.getRed(), snake.getGreen(), snake.getBlue()); //snake color (change)
 	for (const auto& segment : snake.getSegments()) {
 		glBegin(GL_QUADS);
 		glVertex2f(segment.x - size / 2, segment.y - size / 2);
@@ -362,6 +458,10 @@ void init(void)
 	// Set the background color (red, green, blue, alpha)
 	glClearColor(0.3f, 0.5f, 0.2f, 0.5f);
 
+	if (food) {
+		delete food;
+	}
+
 	switch (rand() % 4) {
 	case 0:
 		food = new Apple();
@@ -372,13 +472,12 @@ void init(void)
 	case 2:
 		food = new Grape();
 		break;
-	case 3: 
+	case 3:
 		food = new Banana();
 		break;
 	}
 
 	food->placeRandom(800, 600);
-
 	std::cout << "Finished initializing...\n\n";
 }
 
